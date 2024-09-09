713a8c93e77ff8269bfa2eae9a62e8178aeec9fc
Devy <izabela.marcinkowska@outlook.com>
2024-09-09 16:27:02 +0200

    First commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Izabela Marcinkowska on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
