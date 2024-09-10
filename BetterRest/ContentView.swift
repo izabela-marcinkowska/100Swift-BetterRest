b9e07e3c6f507929c06ee4a6891a1d1c57ad5e5e
Devy <izabela.marcinkowska@outlook.com>
2024-09-10 10:09:25 +0200

    feat: Add wake up time field

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
    
     var body: some View {
+        NavigationStack {
+            
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-           
+          Text("When do you want to wake up?")
+                .font(.headline)
+            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
         }
         .padding()
+        }
     }
 }
 
--- example/example.swift
+++ example/example.swift
@@ -25,11 +25,11 @@ struct example: View {
         .padding()
     }
 }
-func exampleDates() {
-    let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-    let hour = components.hour ?? 0
-    let minute = components.minute ?? 0
-}
+//func exampleDates() {
+//    let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+//    let hour = components.hour ?? 0
+//    let minute = components.minute ?? 0
+//}
 
 #Preview {
     ContentView()
